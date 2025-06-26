f7ee8f96bf57a9214b04deac7bd489fe16870698
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2025-06-26 05:18:49 +0300

    Initial

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,51 @@
+import { ArrowOutward } from '@mui/icons-material';
 import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
+
+const Portfolio = [
+  {
+    id: 1,
+    img: "/pic1.jpg",
+    title: "Peperomia Plants",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptas accusamus.",
+    tags: "Health, Finance, Nutrition, Fitness",
+    lnk: "/one"
+  },
+  {
+    id: 2,
+    img: "/pic1.jpg",
+    title: "Peperomia Plants",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptas accusamus.",
+    tags: "Health, Finance, Nutrition, Fitness",
+    lnk: "https://areteco.agency/"
+  },
+  {
+    id: 3,
+    img: "/pic1.jpg",
+    title: "Peperomia Plants",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptas accusamus.",
+    tags: "Health, Nutrition, Fitness",
+    lnk: "https://areteco.agency/"
+  },
+];
 
 const Main = () => {
   return (
     <>
       <Navbar />
+      <div className="flex flex-wrap gap-[25px] items-center justify-start md:justify-center relative top-[70px] min-h-screen w-screen p-[25px] lg:p-[50px] ">
+        {Portfolio.map((port) => (
+          <div className="flex flex-col items-start justify-between w-[375px] h-[450px] bg-[--SecBgColor] text-[--MainTextColor] bg-opacity-75 backdrop-blur-3xl rounded-[20px] shadow-lg p-3 pb-4" key={port.id}>
+            <img src={port.img} alt="" className='h-[225px] w-full object-cover rounded-[10px]' />
+            <h1 className='font-extrabold text-[2rem] '>{port.title}</h1>
+            <p className='font-light text-opacity-75 '>{port.desc}</p>
+            <div className="flex w-full items-center justify-between">
+              <p className='text-[--SecTextColor] font-bold border-solid border-[2px] border-[--SecTextColor] rounded-lg p-2  '>{port.tags}</p>
+              <Link to={port.lnk} style={{fontWeight:"700"}} className='p-2 bg-[--MainBgColor] shadow-lg rounded-full flex items-center justify-center hover:shadow-2xl hover:scale-[1.05] transition duration-300 '><ArrowOutward /></Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,14 @@ const Navbar = () => {
             <div className='w-full h-[70px] bg-[--SecBgColor] hidden lg:flex items-center justify-between p-[20px] pr-[30px] pl-[30px] fixed z-[99]'>
                 <Link to="/" className="text-3xl font-extrabold text-[--SecTextColor] hover:text-[--MainTextColor]">Arete Co</Link>
                 <div className="flex gap-[25px]">
-                    <Link to="#services" className='hover:text-[--HTextColor]'>Services</Link>
-                    <Link to="#portfolio" className='hover:text-[--HTextColor]'>Portfolio</Link>
-                    <Link to="#about" className='hover:text-[--HTextColor]'>About</Link>
-                    {/* <Link to="#" className='hover:text-[--HTextColor]'>Work</Link> */}
-                    <Link to="#contact" className='hover:text-[--HTextColor]'>Contact</Link>
+                    <Link to="https://areteco.agency/" className='hover:text-[--HTextColor]'>Home</Link>
+                    <Link to="https://areteco.agency/#services" className='hover:text-[--HTextColor]'>Services</Link>
+                    <Link to="https://areteco.agency/#portfolio" className='hover:text-[--HTextColor]'>Portfolio</Link>
+                    <Link to="https://areteco.agency/#about" className='hover:text-[--HTextColor]'>About</Link>
+                    {/* <Link to="https://areteco.agency/#" className='hover:text-[--HTextColor]'>Work</Link> */}
+                    <Link to="https://areteco.agency/#contact" className='hover:text-[--HTextColor]'>Contact</Link>
                 </div>
-                <Link to="/form" className='p-[10px] bg-[--HTextColor] rounded-md font-bold uppercase hover:scale-[1.02]'>Join Now</Link>
+                <Link to="https://areteco.agency/form" className='p-[10px] bg-[--HTextColor] rounded-md font-bold uppercase hover:scale-[1.02]'>Get Started</Link>
             </div>
         </>
     )
